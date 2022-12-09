e5ff772c91170a46a7a04f377ef652c5e0fb9285
jhonnycave <luis.j.cueva@gmail.com>
2022-12-09 18:27:27 -0500

    viernes , revisando

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
 const initialState = {
-	cart: [],
-}
+    cart: [],
+};
 
 const useInitialState = () => {
-	const [state, setState] = useState(initialState);
+    // Hook
+    const [state, setState] = useState(initialState);
+    // Funcion
+    const addToCart = (payload) => {
+        setState({
+            ...state,
+            cart: [...state.cart, payload],
+        });
+    };
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter((items) => items.id !== payload.id),
+        });
+    };
 
-	const addToCart = (payload) => {
-		setState({
-			...state,
-			cart: [...state.cart, payload]
-		});
-	};
-
-	const removeFromCart = (payload) => {
-		setState({
-			...state,
-			cart: state.cart.filter(items => items.id !== payload.id),
-		});
-	}
-
-	return {
-		state,
-		addToCart,
-		removeFromCart
-	}
-}
+    return {
+        state,
+        addToCart,
+        removeFromCart,
+    };
+};
 
 export default useInitialState;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from '@containers/Layout';
-import Home from '@pages/Home';
-import Login from '@pages/Login';
-import PasswordRecovery from '@pages/PasswordRecovery';
-import SendEmail from '@pages/SendEmail';
-import NewPassword from '@pages/NewPassword';
-import MyAccount from '@pages/MyAccount';
-import CreateAccount from '@pages/CreateAccount';
-import Checkout from '@pages/Checkout';
-import Orders from '@pages/Orders';
-import NotFound from '@pages/NotFound';
-import AppContext from '../context/AppContext';
-import useInitialState from '../hooks/useInitialState';
-import '@styles/global.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch as Routes,
+    Route,
+} from "react-router-dom";
+import Layout from "@containers/Layout";
+import AppContext from "../context/AppContext";
+import useInitialState from "../hooks/useInitialState";
+// Components
+import Home from "@pages/Home";
+import Login from "@pages/Login";
+import PasswordRecovery from "@pages/PasswordRecovery";
+import SendEmail from "@pages/SendEmail";
+import NewPassword from "@pages/NewPassword";
+import MyAccount from "@pages/MyAccount";
+import CreateAccount from "@pages/CreateAccount";
+import Checkout from "@pages/Checkout";
+import Orders from "@pages/Orders";
+import NotFound from "@pages/NotFound";
+// Style
+import "@styles/global.css";
 
 const App = () => {
-	const initialState = useInitialState();
-	return (
-		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
-				<Layout>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/password-recovery" component={PasswordRecovery} />
-						<Route exact path="/send-email" component={SendEmail} />
-						<Route exact path="/new-password" component={NewPassword} />
-						<Route exact path="/account" component={MyAccount} />
-						<Route exact path="/signup" component={CreateAccount} />
-						<Route exact path="/checkout" component={Checkout} />
-						<Route exact path="/orders" component={Orders} />
-						<Route path="*" component={NotFound} />
-					</Switch>
-				</Layout>
-			</BrowserRouter>
-		</AppContext.Provider>
-	);
-}
+    const initialState = useInitialState();
+    return (
+        <AppContext.Provider value={initialState}>
+            <Router>
+                <Layout>
+                    <Routes>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/send-email" component={SendEmail} />
+                        <Route path="/new-password" component={NewPassword} />
+                        <Route path="/account" component={MyAccount} />
+                        <Route path="/signup" component={CreateAccount} />
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/orders" component={Orders} />
+                        <Route
+                            path="/password-recovery"
+                            component={PasswordRecovery}
+                        />
+                        <Route path="*" component={NotFound} />
+                    </Routes>
+                </Layout>
+            </Router>
+        </AppContext.Provider>
+    );
+};
 
 export default App;
